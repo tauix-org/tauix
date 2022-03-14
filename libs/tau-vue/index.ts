af633e4921d15c329d238573614587a9ef534092
spikeboss <cajlonbatista@puzzl.com.br>
2022-03-14 19:47:20 -0300

    fix: remove source map from lib vue

--- libs/tau-vue/index.ts
+++ libs/tau-vue/index.ts
@@ -7,15 +7,13 @@ export * from './forerunner/components';
 applyPolyfills().then(() => {
   defineCustomElements(window, {
     ce: (eventName: string, opts: any) => {
-      var eventLower = '';
-
-      for (const ev of eventName) {
-        ev == ev.toUpperCase()
-          ? (eventLower += '-' + ev.toLocaleLowerCase())
-          : (eventLower += ev);
-      }
-
-      return new CustomEvent(eventLower.toLowerCase(), opts);
+      return new CustomEvent(
+        eventName
+          .split('')
+          .map(e => (e == e.toUpperCase() ? '-' + e.toLocaleLowerCase() : e))
+          .join(''),
+        opts
+      );
     },
   } as any);
 });
